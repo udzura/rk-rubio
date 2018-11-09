0d599eb27157cc18d117c874a1d8b2bb1158cc68
Uchio KONDO <udzura@pepabo.com>
2018-11-09 14:22:54 +0900

    First implementation

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "lita"
-gem "lita-slack"
+gem "lita-slack", require: false
 gem "dotenv"
 gem "sinatra"
+gem "lita-scorekeeper", path: "/usr/local/ghq/github.com/udzura/lita-scorekeeper"
--- lita_config.rb
+++ lita_config.rb
@@ -1,10 +1,14 @@
 require "lita"
 require "dotenv"
-require "lita-slack"
 Dotenv.load
 
 Lita.configure do |config|
-  config.robot.adapter = :slack
-  config.adapters.slack.token = ENV['SLACK_TOKEN']
-  config.redis[:host] = ENV['REDIS_HOST']
+  if ENV['DEBUG']
+    config.robot.adapter = :shell
+  else
+    require "lita-slack"
+    config.robot.adapter = :slack
+    config.adapters.slack.token = ENV['SLACK_TOKEN']
+  end
+  config.redis[:host] = ENV['REDIS_HOST'] || "localhost"
 end
